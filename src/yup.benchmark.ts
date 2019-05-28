fa098e447373cc6ab37be8a791372a106327a727
Artem Anashev <anashev@yamoney.ru>
2019-05-28 15:51:06 +0300

    Add "chalk" and "cli-table" modules and print demo table

--- src/yup.benchmark.ts
+++ src/yup.benchmark.ts
@@ -1,6 +1,34 @@
 import * as Benchmark from 'benchmark';
+import Chalk from 'chalk';
+// @ts-ignore
+import * as CliTable from 'cli-table';
+import * as D3 from 'd3-format';
 
-Benchmark.options.minSamples = 100;
+enum Formats {
+	// @ts-ignore
+	Number = D3.format(',d'),
+	// @ts-ignore
+	Percentage = D3.format('.2f'),
+	// @ts-ignore
+	Integer = D3.format(',')
+}
+
+Benchmark.options.minSamples = 1;
+
+interface Report {
+	/** Report name */
+	name: string;
+
+	/** The number of executions per second */
+	hz: number;
+
+	/** The relative margin of error */
+	rme: number;
+
+	/** Array of count sampled periods */
+	size: number;
+	error?: string;
+}
 
 const Suite = new Benchmark.Suite('yup');
 
@@ -11,11 +39,71 @@ Suite
 	.add('String#indexOf', () => {
 		return 'Hello World!'.indexOf('o') > -1;
 	})
-	.on('cycle', function(event) {
+	.on('cycle', (event) => {
 		console.log(String(event.target));
 	})
-	.on('complete', function() {
+	.on('complete', function(benches) {
+		const reports: Array<Report> = benches.currentTarget.map((bench) => {
+			return {
+				name: bench.name,
+				hz: bench.hz,
+				rme: bench.stats.rme,
+				size: bench.stats.sample.length,
+				error: bench.error
+			};
+		});
+
+		const columnAligns: Array<'left' | 'middle' | 'right'> = [
+			'left',
+			'left',
+			'right',
+			'right',
+			'right'
+		];
+
+		const tableDefinition = {
+			head: [
+				Chalk.blue('Name'),
+				Chalk.yellow('Ops/sec'),
+				Chalk.yellow('MoE'),
+				Chalk.yellow('Runs sampled')
+			],
+			colAligns: columnAligns
+		};
+
+		const table = new CliTable(tableDefinition);
+
+		const slowestReportHz = Math.min(...reports.map((report) => report.hz));
+		table.push(...reports.map((report) => {
+			const isSlowertReport = report.hz === slowestReportHz;
+
+			const reportName = ((isSlowertReport) => {
+				if (isSlowertReport) {
+					return Chalk.redBright(report.name);
+				}
+
+				if (report.error) {
+					return Chalk.redBright(report.name);
+				}
+
+				return report.name;
+			})(isSlowertReport);
+
+			const row = [
+				reportName,
+				// @ts-ignore
+				Formats.Number(report.hz),
+				// @ts-ignore
+				`± ${Formats.Percentage(report.rme)} %`,
+				// @ts-ignore
+				Formats.Integer(report.size)
+			];
+
+			return row;
+		}));
+
 		console.log('Fastest is ' + this.filter('fastest').map('name'));
+		console.log(table.toString());
 	})
 	// run async
 	.run({ 'async': true })
